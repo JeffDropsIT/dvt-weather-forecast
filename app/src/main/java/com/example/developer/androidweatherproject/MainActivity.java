8ddbe00d8a5790ad55ab3d9819a8844c357420db
JeffDropsIT <mathonsiphumzile@gmail.com>
2018-10-09 22:39:39 +0200

    cleaned mainactivity getloaction function and permision checks

--- app/src/main/java/com/example/developer/androidweatherproject/MainActivity.java
+++ app/src/main/java/com/example/developer/androidweatherproject/MainActivity.java
@@ -83,12 +83,7 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        getLastKnownLocation(new OnlocationListener() {
-            @Override
-            public void onLocationComplete() {
-
-            }
-        });
+        checkPermission();
         connectivityManager
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         //shared prefs value to check whether data has been cached
@@ -96,11 +91,6 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
 
         if (!isNetworkAvailable() && !getBoolean(IS_CACHED)) {
             startErrorActivity();
-            //setContentView(R.layout.error_layout);
-            putString("layout", "error_layout");
-        } else {
-
-            putString("layout", "activity_main");
         }
 
 
@@ -195,41 +185,8 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
         preferences.edit().putString(key, value).apply();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_ACCESS_LOCATION_PERMISSIONS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    Log.i("WSX", "onRequestPermissionsResult: Granted");
-                    getLastKnownLocation(new OnlocationListener() {
-                        @Override
-                        public void onLocationComplete() {
-                            Log.i("WSX", "onRequestPermissionsResult: Granted");
-                            if (isNetworkAvailable()) {
-                                new HttpRequestTask(MainActivity.this).execute(getString("lat"),getString("lon") );
-                                Log.i("WSX", "onCreate: internet connection ");
-                            } else {
-                                Log.i("WSX", "onCreate: no internet connection ");
-                            }
-                        }
-                    });
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
 
-            // other 'case' lines to check for other
-            // permissions this app might request.
-     }
-    }
-
-    private void getLastKnownLocation(final OnlocationListener onlocationListener) {
+    private void checkPermission(){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
@@ -256,11 +213,12 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
                         REQUEST_ACCESS_LOCATION_PERMISSIONS);
 
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
+    }
+
+    private void getLastKnownLocation(final OnlocationListener onlocationListener) {
+        checkPermission();
         mFusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
@@ -342,42 +300,6 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
     }
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(isNetworkAvailable()){
-            if(!MainActivity.getString("lat").equals("0") && !MainActivity.getString("lon" ).equals("0")){
-                Log.i("WSX", "onResume: lat "+getString("lat")+" lon "+getString("lon"));
-                new HttpRequestTask(this).execute(getString("lat"),getString("lon"));
-            }else {
-                getLastKnownLocation(new OnlocationListener() {
-                    @Override
-                    public void onLocationComplete() {
-
-                    }
-                });
-            }
-            Log.i("WSX", "onResume: lat "+getString("lat")+" lon "+getString("lon"));
-           // new HttpRequestTask(this).execute("-29.844776","31.014339");
-            Log.i("WSX", "onResume: internet connection ");
-        }else{
-            Log.i("WSX", "onResume: no internet connection ");
-        }
-
-        if(getBoolean(IS_CACHED)){
-            updateUI();
-        }
-    }
-
-    private boolean isWeatherLayoutPresent(){
-        return  getString("layout").contains("activity_main");
-    }
 
     private void restartActivity(){
         if(Build.VERSION.SDK_INT > 11){
@@ -394,26 +316,6 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
         }
     }
 
-    private void setLayoutViews(){
-        updateWeatherInfo();
-        displayWeekDays();
-        displayWeatherInfo();
-        setWeekForecastIcons();
-        setWeekForecastTemps();
-    }
-    private void updateUI(){
-
-        getWeatherForecastList = storageDBServer.getWeatherForecast();
-        if(isWeatherLayoutPresent()){
-            setLayoutViews();
-        }else{
-            if(isNetworkAvailable() && getBoolean(IS_CACHED)){
-                restartActivity();
-            }
-
-        }
-
-    }
     private int getCurrentHour(){
 
         Date currentForecastTmp =  new Date();
@@ -460,6 +362,30 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
     }
 
 
+    private int[] calculateForecastHours(){
+        int currentHour  = getCurrentHour();
+        int remainderNxtHour = currentHour % 3;
+        int currentForecastHour = currentHour;
+
+        //get the current time and calculate the next hour, data needs
+        //to be updated and set alarm manager to get refreshed on current time
+        if(remainderNxtHour == 0){
+            currentHour += 3;
+
+        }else if(remainderNxtHour == 1){
+            currentHour += 2;
+            currentForecastHour -= 1;
+
+        }else if(remainderNxtHour == 2){
+            currentHour += 1;
+            currentForecastHour -= 2;
+        }
+        int futureForecastHour = currentHour;
+        Log.i("WSX", "calculateForecastHours: futureForecastHour hour "+futureForecastHour);
+        Log.i("WSX", "calculateForecastHours: currentForecastHour hour "+currentForecastHour);
+        return new int[]{ futureForecastHour , currentForecastHour};
+    }
+
     private void setAppWeatherTheme(String main){
         if(main.contains(CLEAR)){
             ImgCoverImage.setImageResource(R.drawable.forest_sunny);
@@ -486,28 +412,20 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
         }
     }
 
-    private int[] calculateForecastHours(){
-        int currentHour  = getCurrentHour();
-        int remainderNxtHour = currentHour % 3;
-        int currentForecastHour = currentHour;
 
-        //get the current time and calculate the next hour, data needs
-        //to be updated and set alarm manager to get refreshed on current time
-        if(remainderNxtHour == 0){
-            currentHour += 3;
+    private void setLayoutViews(){
+        updateWeatherInfo();
+        displayWeekDays();
+        displayWeatherInfo();
+        setWeekForecastIcons();
+        setWeekForecastTemps();
+    }
+    private void updateUI(){
 
-        }else if(remainderNxtHour == 1){
-            currentHour += 2;
-            currentForecastHour -= 1;
+        getWeatherForecastList = storageDBServer.getWeatherForecast();
+        setLayoutViews();
 
-        }else if(remainderNxtHour == 2){
-            currentHour += 1;
-            currentForecastHour -= 2;
-        }
-        int futureForecastHour = currentHour;
-        Log.i("WSX", "calculateForecastHours: futureForecastHour hour "+futureForecastHour);
-        Log.i("WSX", "calculateForecastHours: currentForecastHour hour "+currentForecastHour);
-        return new int[]{ futureForecastHour , currentForecastHour};
+
     }
     private void updateWeatherInfo(){
 
@@ -618,10 +536,75 @@ public class MainActivity extends AppCompatActivity implements HttpRequestTask.O
         startActivity(errorIntent);
         finish();
     }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isNetworkAvailable()){
+            if(!MainActivity.getString("lat").equals("0") && !MainActivity.getString("lon" ).equals("0")){
+                Log.i("WSX", "onResume: lat "+getString("lat")+" lon "+getString("lon"));
+                new HttpRequestTask(this).execute(getString("lat"),getString("lon"));
+            }else {
+                getLastKnownLocation(new OnlocationListener() {
+                    @Override
+                    public void onLocationComplete() {
+
+                    }
+                });
+            }
+            Log.i("WSX", "onResume: lat "+getString("lat")+" lon "+getString("lon"));
+            // new HttpRequestTask(this).execute("-29.844776","31.014339");
+            Log.i("WSX", "onResume: internet connection ");
+        }else{
+            Log.i("WSX", "onResume: no internet connection ");
+        }
+
+        if(getBoolean(IS_CACHED)){
+            updateUI();
+        }
+    }
+
     @Override
     public void onTaskFailed() {
         startErrorActivity();
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_ACCESS_LOCATION_PERMISSIONS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.i("WSX", "onRequestPermissionsResult: Granted");
+                    getLastKnownLocation(new OnlocationListener() {
+                        @Override
+                        public void onLocationComplete() {
+                            Log.i("WSX", "onRequestPermissionsResult: Granted");
+                            if (isNetworkAvailable()) {
+                                new HttpRequestTask(MainActivity.this).execute(getString("lat"),getString("lon") );
+                                Log.i("WSX", "onCreate: internet connection ");
+                            } else {
+                                Log.i("WSX", "onCreate: no internet connection ");
+                            }
+                        }
+                    });
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+
 
     public interface OnlocationListener{
          void onLocationComplete();
