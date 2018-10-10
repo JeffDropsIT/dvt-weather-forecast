310814e95cdad7a5b026a84aa1693ba291e98228
JeffDropsIT <mathonsiphumzile@gmail.com>
2018-10-10 20:00:36 +0200

    fixed screensized for all devices, added screen refresh layout

--- app/src/main/java/com/example/developer/androidweatherproject/SomethingWentWrongActivity.java
+++ app/src/main/java/com/example/developer/androidweatherproject/SomethingWentWrongActivity.java
@@ -1,12 +1,16 @@
 package com.example.developer.androidweatherproject;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SomethingWentWrongActivity extends AppCompatActivity {
 
@@ -15,10 +19,29 @@ public class SomethingWentWrongActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.error_layout);
 
+        setStatusBar(R.color.colorDarkGrey);
+        refreshLayout(); //swipe down lo refresh layout
 
     }
+    private void setStatusBar(int color){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(color));
+        }
+    }
 
+    private void refreshLayout(){
+        final SwipeRefreshLayout pullToRefresh = findViewById(R.id.srl_layout);
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                onNetworkFound();
+                pullToRefresh.setRefreshing(false);
+            }
+        });
 
+    }
     private void onNetworkFound(){
         if(MainActivity.isNetworkAvailable() ){
             startMainActivity();
